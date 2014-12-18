07a5d25229e6e0d666563d93307b74ebfcd174e9
Tr1ckX <attila.mirk@gmail.com>
2014-12-18 15:47:34 +0000

    star rating developed

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -11,15 +11,18 @@ feature 'reviewing' do
     Restaurant.create name: 'KFC'
   end
 
-  scenario 'allows users to leave a review using a form' do
+  def leave_review(thoughts, rating)
     visit '/restaurants'
     click_link 'Review KFC'
-    fill_in "Thoughts", with: "so so"
-    select '3', from: 'Rating'
+    fill_in 'Thoughts', with: thoughts
+    select rating, from: 'Rating'
     click_button 'Leave Review'
+  end
 
-    expect(current_path).to eq '/restaurants'
-    expect(page).to have_content('so so')
+  scenario 'displays an average rating for all reviews' do
+    leave_review('So so', '3')
+    leave_review('Great', '5')
+    expect(page).to have_content('Average rating: ★★★★☆')
   end
 
 end
