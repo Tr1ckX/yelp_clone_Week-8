c54f5eb4012f1128dc4e207eae0d36315eed24d9
Tr1ckX <attila.mirk@gmail.com>
2014-12-18 12:03:09 +0000

    database fix

--- db/migrate/20141217170318_remove_description_from_reviews.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDescriptionFromReviews < ActiveRecord::Migration
-  def change
-    remove_column :reviews, :description, :integer
-  end
-end
--- db/migrate/20141217171615_remove_review_id_from_reviews.rb
+++
@@ -1,5 +0,0 @@
-class RemoveReviewIdFromReviews < ActiveRecord::Migration
-  def change
-    remove_column :reviews, :review_id, :integer
-  end
-end
